6f6a07daf06eaa8ef4721fc84ca1c862752d7f13
201411090 <sweetick5@gmail.com>
2018-05-29 19:51:45 +0900

    #1190 할일 정렬 기능 구현

--- src/main/java/se/smu/Print_Show_Sorted_List_0514_v1_6_SIXSENSE.java
+++ src/main/java/se/smu/Print_Show_Sorted_List_0514_v1_6_SIXSENSE.java
@@ -3,19 +3,19 @@ package se.smu;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Label;
-import java.awt.Panel;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
-import javax.swing.JScrollBar;
-import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.JList;
 
 public class Print_Show_Sorted_List_0514_v1_6_SIXSENSE extends JPanel{
 	JPanelMain panel;
@@ -23,28 +23,55 @@ public class Print_Show_Sorted_List_0514_v1_6_SIXSENSE extends JPanel{
 	JPanel Finished_Panel = new JPanel();	//클래스 설계서에 없는거 
 	JLabel Unfinished_todo_Label = new JLabel("\uBBF8\uC644\uB8CC \uD56D\uBAA9");
 	JLabel Finished_todo_Label = new JLabel("\uC644\uB8CC\uB41C \uD56D\uBAA9");
-	String sortPolicy[] = {"정렬기준1", "정렬기준2", "정렬기준3"};	//내용에맞게 바꿔야됨
+	String sortPolicy[] = {"과목 명", "마감 기한", "실제 마감일"};	
 	JComboBox Sort_Standard_Combobox = new JComboBox(sortPolicy);
 	Button Prev_Button = new Button("Prev");
+	
+	static DefaultListModel<String> UnfinishedModel = new DefaultListModel();
+	static DefaultListModel<String> FinishedModel = new DefaultListModel();
 	private Font ButtonFont = new Font("Arial Unicode MS", Font.PLAIN, 12);
 	private Font LableFont = new Font("Arial Unicode MS", Font.PLAIN, 24);
-	private Font TextFont = new Font("Arial Unicode MS", Font.PLAIN, 14);
 	
+	JList Unfinished_List = new JList(UnfinishedModel);
+	JList Finished_List = new JList(FinishedModel);
+	static String wholeTodo=""; //구분자 ;
+	String[] Todos;
+	String[] TodoComponents;
+	 
 	public Print_Show_Sorted_List_0514_v1_6_SIXSENSE(final JPanelMain panel) {
 		setBackground(Color.WHITE);
 		this.panel=panel;
 		setLayout(null);		
+		
+		try {
+			loadTodo();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		splitTodos();
+		
 		
 		Unfinished_Panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		Unfinished_Panel.setBackground(Color.WHITE);
 		Unfinished_Panel.setBounds(2, 50, 356, 230);
 		add(Unfinished_Panel);
 		Unfinished_Panel.setLayout(null);
+		Unfinished_List.setFont(new Font("Dialog", Font.PLAIN, 15));
+		Unfinished_List.setBounds(0, 0, 356, 230);
+		
+		Unfinished_Panel.add(Unfinished_List);
 		
 		Finished_Panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		Finished_Panel.setBackground(Color.WHITE);
 		Finished_Panel.setBounds(2, 338, 356, 202);
 		add(Finished_Panel);
+		Finished_Panel.setLayout(null);
+		Finished_List.setFont(new Font("Dialog", Font.PLAIN, 15));
+		Finished_List.setBounds(0, 0, 356, 202);
+		
+		Finished_Panel.add(Finished_List);
 		
 		Unfinished_todo_Label.setFont(LableFont);
 		Unfinished_todo_Label.setBounds(0, 0, 142, 38);
@@ -58,18 +85,19 @@ public class Print_Show_Sorted_List_0514_v1_6_SIXSENSE extends JPanel{
 		
 		Sort_Standard_Combobox.setBounds(210, 7, 135, 38);
 		add(Sort_Standard_Combobox);
-		Sort_Standard_Combobox.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				
-				
-			}
-		});
+	
 		
 		Prev_Button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				try {
+					this.finalize();
+				} catch (Throwable e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				panel.change("Print_Subject_List_0511_v1_6_SIXSENSE");	//prev 눌렀을때 이전화면으로 돌아가버튼
+				
+				
 			}
 		});
 		Prev_Button.setFont(ButtonFont);
@@ -79,7 +107,144 @@ public class Print_Show_Sorted_List_0514_v1_6_SIXSENSE extends JPanel{
 		add(Prev_Button);
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(0, 0, 360, 640);
-
+		
+		
+		Sort_Standard_Combobox.addActionListener(new MyComboboxActionListener());
+		
+		
+		
+		
 		
 	}
+	
+	public class MyComboboxActionListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Sort_Standard_Combobox =  (JComboBox) e.getSource();
+			int index = Sort_Standard_Combobox.getSelectedIndex();
+			System.out.println(wholeTodo);
+			splitTodos();
+			for(int i=0; i<Todos.length; i++) {
+			System.out.println(Todos[i] + "\n");
+			}
+			changeComponentsLocation(index);
+			clearList();
+			updateList();
+		}
+		
+	}
+	
+	public void saveFile() throws IOException {
+	
+	}
+	
+	public static void loadTodo() throws IOException{ //txt에서 입력받기 main 시작할때 호출 개행단위로 읽어서 add+todo() 실행
+		clearList();
+		String PackagePath = Print_Show_Sorted_List_0514_v1_6_SIXSENSE.class.getResource("").getPath();	
+		String TodoTextPath = PackagePath + "Todo.txt";
+		
+		 BufferedReader br = new BufferedReader(new FileReader(TodoTextPath));
+		 String[] tempStr1;
+		 String Status="";
+		
+	        while(true) {
+	            String line = br.readLine();
+	            if (line==null) break;
+	            wholeTodo = wholeTodo + line + ";";
+	            
+	            tempStr1 = line.split("/");
+	           
+	           	Status = tempStr1[1];
+
+	         	if(Status.equals("미완료")) {
+	         			UnfinishedModel.addElement(line);
+	         			
+  	     
+  	            }
+  	           	else if(Status.equals("완료")) {
+  	           		FinishedModel.addElement(line);
+  	           		
+
+	         	}
+	        }
+	        br.close();
+	    }
+	
+	public void splitTodos() {
+		System.out.println("wholeTodo : " + wholeTodo);
+		Todos = wholeTodo.split(";");
+	}
+		
+		
+	
+	public void changeComponentsLocation(int sortPolicy) {
+		int len = Todos.length;
+		
+		for(int i=0; i<len; i++) {
+			TodoComponents = Todos[i].split("/");
+			String temp;
+			switch(sortPolicy) {
+			case 0 : 
+				temp = TodoComponents[4];
+				TodoComponents[4] = TodoComponents[0];
+				TodoComponents[0] = temp;
+				break;
+			case 1 : 
+				temp = TodoComponents[3];
+				TodoComponents[3] = TodoComponents[0];
+				TodoComponents[0] = temp;
+				break;
+			case 2 : 
+				temp = TodoComponents[5];
+				TodoComponents[5] = TodoComponents[0];
+				TodoComponents[0] = temp;
+			}
+			
+			Todos[i] = TodoComponents[0] + "/" + TodoComponents[1] + "/" + TodoComponents[2] + 
+					"/" + TodoComponents[3] + "/" + TodoComponents[4] + "/" +TodoComponents[5];
+		}
+	}
+	
+	public static void clearList() {
+		
+		UnfinishedModel.removeAllElements();
+		FinishedModel.removeAllElements();		
+	}
+	
+	public void updateList() {
+		 String[] tempStr1;
+		 String Status="";
+		 
+		 System.out.println(Todos.length);
+		 Arrays.sort(Todos);
+		 
+		 for(int i=0; i<Todos.length; i++) {
+			 System.out.println(Todos[i]);
+		 }
+		 for(int i=0; i<Todos.length; i++) {
+		
+			 tempStr1 = Todos[i].split("/");
+			 
+			 Status = tempStr1[1];
+			 
+			 if(Status.equals("미완료")) {
+      			UnfinishedModel.addElement(Todos[i]);
+    
+           }
+          	else if(Status.equals("완료")) {
+          		FinishedModel.addElement(Todos[i]);
+
+      	}
+			 
+		 }
+		
+		 
+		 
+	}
+	
+	
+	
+	
+	
 }
