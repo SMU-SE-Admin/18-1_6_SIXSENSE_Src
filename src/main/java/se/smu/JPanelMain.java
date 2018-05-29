dc398bd656c81eabdd5644579653f1860d915578
201411090 <sweetick5@gmail.com>
2018-05-29 11:08:41 +0900

    #1183 메인 페이지 구현

--- src/main/java/se/smu/JPanelMain.java
+++ src/main/java/se/smu/JPanelMain.java
@@ -20,14 +20,14 @@ public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFr
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("To Do List by SIXSNESE");
 	}
-    public Print_Add_Todo1 Paddtodo1 = null; //각화면 클래스를 등록할 예정
-    public Print_Add_Todo2 Paddtodo2 = null;
+    //public Print_Add_Todo1 Paddtodo1 = null; //각화면 클래스를 등록할 예정
+    //public Print_Add_Todo2 Paddtodo2 = null;
     public Print_Show_Sorted_List_0514_v1_6_SIXSENSE Pshowsortedlist = null;
     public Print_Login_0510_v1_6_SIXSENSE Printlogin = null;
     public Print_Subject_List_0511_v1_6_SIXSENSE PsubjectList = null;    
     
     public void change(String panelName) {
-       if(panelName.equals("Print_Add_Todo1")) { //출력할 화면을 재지정
+      /* if(panelName.equals("Print_Add_Todo1")) { //출력할 화면을 재지정
           getContentPane().removeAll();
           getContentPane().add(Paddtodo1);
           revalidate();
@@ -38,9 +38,9 @@ public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFr
           getContentPane().add(Paddtodo2);
           revalidate();
           repaint();
-       }
+       }*/
 
-       else if(panelName.equals("Print_Show_Sorted_List_0514_v1_6_SIXSENSE")) {
+       if(panelName.equals("Print_Show_Sorted_List_0514_v1_6_SIXSENSE")) {
     	   getContentPane().removeAll();
            getContentPane().add(Pshowsortedlist);
            revalidate();
@@ -60,8 +60,8 @@ public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFr
     public static void main(String[] args) {
        JPanelMain main= new JPanelMain();
        main.setTitle("To Do List_SIXSENSE");
-       main.Paddtodo1= new Print_Add_Todo1(main); 
-       main.Paddtodo2= new Print_Add_Todo2(main);
+       //main.Paddtodo1= new Print_Add_Todo1(main); 
+       //main.Paddtodo2= new Print_Add_Todo2(main);
        main.Printlogin = new Print_Login_0510_v1_6_SIXSENSE(main);
        main.Pshowsortedlist = new Print_Show_Sorted_List_0514_v1_6_SIXSENSE(main);
        main.PsubjectList = new Print_Subject_List_0511_v1_6_SIXSENSE(main);
