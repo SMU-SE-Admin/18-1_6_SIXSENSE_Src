6303dbc544562f7a0fdd5a7bfcef3ae1b4424383
201411090 <sweetick5@gmail.com>
2018-05-31 11:29:15 +0900

    #1339 Edit 기능, #1344 상태변경, #1344 상태변경(2) 오류 결함 해결

--- src/main/java/se/smu/Print_Add_Edit_Todo_temp.java
+++ src/main/java/se/smu/Print_Add_Edit_Todo_temp.java
@@ -4,7 +4,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-//PMD Rule 위반 수정
 public class Print_Add_Edit_Todo_temp extends JFrame{
 	static public String[] Todo_Info=new String[2];
 	 boolean addfile;
@@ -14,25 +13,24 @@ public class Print_Add_Edit_Todo_temp extends JFrame{
 		// TODO Auto-generated constructor stub
 		JTextField Todo_Name_TextField = new JTextField();
 		JTextField Todo_Deadline_TextField = new JTextField();
-		//JTextField Todo_NickName_TextField = new JTextField();
+		
 		
 		
 		Object[] message = {
 			    "항목명", Todo_Name_TextField,
 			    "마감 기한", Todo_Deadline_TextField,
-			   //"과목 별칭", Todo_NickName_TextField,
 			};
 
 		//1 : edit
 				if(addOrEdit==1) {
 					Todo_Name_TextField.setText(Todo_Info[0]);
 					Todo_Deadline_TextField.setText(Todo_Info[1]);
-					//Todo_NickName_TextField.setText(Todo_Info[2]);
+					
 				}
 				else {
 					Todo_Name_TextField.setText("");
 					Todo_Deadline_TextField.setText("");
-					//Todo_NickName_TextField.setText("");
+					
 				}
 		String nameOftheWindow;
 		if(addOrEdit == 0)
@@ -48,11 +46,11 @@ public class Print_Add_Edit_Todo_temp extends JFrame{
 		if (option == JOptionPane.OK_OPTION) {
 			Todo_Info[0] = Todo_Name_TextField.getText();
 			Todo_Info[1] = Todo_Deadline_TextField.getText();
-			//Todo_Info[2] = Todo_NickName_TextField.getText();
+		
 		
 			for(int i=0;i<2;i++) {
-				 if(Todo_Info[i].equals("")) {
-					 JOptionPane.showMessageDialog(null, "모든항목을 입력하세요.");
+				 if(Todo_Info[i].equals("") || Todo_Info[i].contains("/")) {
+					 JOptionPane.showMessageDialog(null, "잘못된 입력입니다.(공백 및 '/' 금지)");
 					 isCreateNewNode.isCreateNewNode=false;
 					 break;
 				 }
