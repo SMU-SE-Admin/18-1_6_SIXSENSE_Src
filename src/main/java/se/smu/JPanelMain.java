d777ea13489ad1a2bf85616f9a715931a1ed819b
201411090 <sweetick5@gmail.com>
2018-06-01 14:27:02 +0900

    #1183 메인페이지 소스코드 최종본 업로드

--- src/main/java/se/smu/JPanelMain.java
+++ src/main/java/se/smu/JPanelMain.java
@@ -6,7 +6,6 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 
-//PMD Rule 위반 수정
 public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFrame {
 	public JPanelMain() {
 		this.setSize(360, 640);
@@ -20,26 +19,12 @@ public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFr
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("To Do List by SIXSNESE");
 	}
-    //public Print_Add_Todo1 Paddtodo1 = null; //각화면 클래스를 등록할 예정
-    //public Print_Add_Todo2 Paddtodo2 = null;
+
     public Print_Show_Sorted_List_0514_v1_6_SIXSENSE Pshowsortedlist = null;
     public Print_Login_0510_v1_6_SIXSENSE Printlogin = null;
     public Print_Subject_List_0511_v1_6_SIXSENSE PsubjectList = null;    
     
     public void change(String panelName) {
-      /* if(panelName.equals("Print_Add_Todo1")) { //출력할 화면을 재지정
-          getContentPane().removeAll();
-          getContentPane().add(Paddtodo1);
-          revalidate();
-          repaint();
-       }
-       else if(panelName.equals("Print_Add_Todo2")) {
-          getContentPane().removeAll();
-          getContentPane().add(Paddtodo2);
-          revalidate();
-          repaint();
-       }*/
-
        if(panelName.equals("Print_Show_Sorted_List_0514_v1_6_SIXSENSE")) {
     	   getContentPane().removeAll();
            getContentPane().add(Pshowsortedlist);
@@ -60,9 +45,7 @@ public class JPanelMain<Print_Add_Subject_Screen_0515_v1_6_SIXSENSE> extends JFr
     public static void main(String[] args) {
        JPanelMain main= new JPanelMain();
        main.setTitle("To Do List_SIXSENSE");
-       //ain.Paddtodo1= new Print_Add_Todo1(main); 
-       //main.Paddtodo2= new Print_Add_Todo2(main);
-       //
+     
        main.Printlogin = new Print_Login_0510_v1_6_SIXSENSE(main);
        main.Pshowsortedlist = new Print_Show_Sorted_List_0514_v1_6_SIXSENSE(main);
        main.PsubjectList = new Print_Subject_List_0511_v1_6_SIXSENSE(main);
